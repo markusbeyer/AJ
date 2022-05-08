0869d1a66a08ce1cbdf6c2fdab04f9d4467f65f6
Markus Beyer <markus.beyer10969@web.de>
2022-05-08 08:12:51 +0200

    Created basic code

---
+++ AJ.py
@@ -0,0 +1,8 @@
+import sys, os, time, datetime
+import docx                                                                                            
+from   docx.shared          import RGBColor, Pt                                                      
+from   docx.enum.text       import WD_ALIGN_PARAGRAPH
+from   colorama             import *
+from   art                  import *
+################################################################################################################# A  J #####################################################################################################################
+# should be able to automatically write journal entry to a file with timestamp
