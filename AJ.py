c1a6d7b4006154d01a23b065720cabaf1a4d5969
Markus Beyer <markus.beyer10969@web.de>
2022-05-08 15:52:18 +0200

    Added some more code. almost done

--- AJ.py
+++ AJ.py
@@ -46,4 +46,43 @@ def AJ():
        print("")
        print(now+Style.RESET_ALL)
        newentry = input("")
+       if   os.path.exists("Archives\\AJ.docx") == True:
+              file              = docx.Document("Archives\\AJ.docx")
+              AJ1               = file.add_paragraph().add_run(now)
+              AJ2               = file.add_paragraph()
+              AJ3               = AJ2.add_run(newentry) 
+              AJ1.bold          = True
+              AJ3.add_break()
+              file.save("Archives\\AJ.docx")
+       elif os.path.exists("Archives\\AJ.docx") == False:
+              file              = docx.Document()
+              AJ0               = file.add_paragraph().add_run("                     AJ")
+              AJ0.font.size     = Pt(41)
+              AJ0.bold          = True
+              AJ0run            = file.add_paragraph().add_run("______________________________________________")
+              AJ0run.font.size  = Pt(25)
+              AJ0run.bold       = True
+              AJ1               = file.add_paragraph().add_run()
+              AJ1.add_break()
+              AJ1.add_break()
+              AJ2               = file.add_paragraph().add_run(now)
+              AJ3               = file.add_paragraph().add_run(newentry) 
+              AJ2.bold          = True
+              AJ3.add_break()
+              file.save("Archives\\AJ.docx")
+       print(clear)
+       print("\n"*20)
+       print(Fore.LIGHTGREEN_EX+"")
+       tprint("       AJ ENTRY SAVED!", font="cyber")
+       if os.path.exists('log.txt') == False:
+           log = open('log.txt','w')
+           log.write(now + " |  New AJ-Entry.")
+           log.close()
+       elif os.path.exists('log.txt') == True:
+           log = open('log.txt','a')
+           log.write("\n" + now + " |  New AJ-Entry.")
+           log.close()
+       print(""+Style.RESET_ALL)
+       time.sleep(0.5)
        
+
